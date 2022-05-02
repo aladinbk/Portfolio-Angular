dcb1b8ea6da94b6cbde1be80ecca79b5162bd4c2
Alaeddine boubaker <alaeddineboubaker@gmail.com>
2022-05-02 20:28:21 +0100

    make it better

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/monportfolioangular'));
+app.use(express.static('./dist/portfolioherokuapp/index.html'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/monportfolioangular/'}),
+app.get('./*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/portfolioherokuapp/'}),
 );
 
 // Start the app by listening on the default Heroku port
