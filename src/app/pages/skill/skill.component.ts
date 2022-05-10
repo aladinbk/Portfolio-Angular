f11041584f31045aacd6a4305164a8053d691561
Alaeddine boubaker <alaeddineboubaker@gmail.com>
2022-05-10 04:45:39 +0100

    changes and add

--- src/app/pages/skill/skill.component.ts
+++ src/app/pages/skill/skill.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillComponent implements OnInit {
   public openList = 'frontend';
+  widht1:any;
+  widht2:any;
+  widht3:any;
+  widht4:any;
+  widht5:any;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.widht1=90;
+    this.widht2=70;
+    this.widht3=60;
+    this.widht4=70;
+    this.widht5=50;
+  }
 
   cambioOpenLIst(nameList: string): void {
     this.openList = nameList;
