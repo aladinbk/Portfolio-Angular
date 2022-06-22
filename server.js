1e8cfff49888e3f38d6cfc9d6c743f7e58678685
Alaeddine boubaker <alaeddineboubaker@gmail.com>
2022-06-22 03:30:58 +0100

    deploy app in heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/portfolioherokuapp'));
+app.use(express.static('./dist/scroll-page/aladin-portfolio'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/portfolioherokuapp/'}),
+    res.sendFile('index.html', {root: 'dist/scroll-page/aladin-portfolio/'}),
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
